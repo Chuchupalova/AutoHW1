2bedcd22fb480c573c69c544d48595d7cb61ec4f
ElenaCh <le_ku@live.com>
2025-10-30 15:34:29 +0200

    Fix homework_04.py

--- homework_04.py
+++ homework_04.py
@@ -1,24 +1,17 @@
-adwentures_of_tom_sawer = """\
-Tom gave up the brush with reluctance in his .... face but alacrity
-in his heart. And while
-the late steamer
-"Big Missouri" worked ....
-and sweated
-in the sun,
-the retired artist sat on a barrel in the .... shade close by, dangled his legs,
-munched his apple, and planned the slaughter of more innocents.
-There was no lack of material;
-boys happened along every little while;
-they came to jeer, but .... remained to whitewash. ....
-By the time Ben was fagged out, Tom had traded the next chance to Billy Fisher for
-a kite, in good repair;
-and when he played
-out, Johnny Miller bought
-in for a dead rat and a string to swing it with—and so on, and so on,
-hour after hour. And when the middle of the afternoon came, from being a
-poor poverty, stricken boy in the .... morning, Tom was literally
-rolling in wealth."""
-
+adwentures_of_tom_sawer = (
+    "Tom gave up the brush with reluctance in his .... face but alacrity "
+    "in his heart. And while the late steamer "
+    "\"Big Missouri\" worked .... and sweated in the sun, "
+    "the retired artist sat on a barrel in the .... shade close by, dangled his legs, "
+    "munched his apple, and planned the slaughter of more innocents. "
+    "There was no lack of material; boys happened along every little while; "
+    "they came to jeer, but .... remained to whitewash. .... "
+    "By the time Ben was fagged out, Tom had traded the next chance to Billy Fisher for "
+    "a kite, in good repair; and when he played out, Johnny Miller bought in "
+    "for a dead rat and a string to swing it with—and so on, and so on, "
+    "hour after hour. And when the middle of the afternoon came, from being a "
+    "poor poverty, stricken boy in the .... morning, Tom was literally rolling in wealth."
+)
 ##  ПЕРЕЗАПИСУЙТЕ зміст змінної adwentures_of_tom_sawer у завданнях 1-3
 # task 01 ==
 """ Дані у строці adwentures_of_tom_sawer розбиті випадковим чином, через помилку.
